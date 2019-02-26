e77c50f2f5c6b00df29088de65d205f29303da8d
Mick Vleeshouwer <mick@imick.nl>
2019-02-26 23:53:55 +0100

    Uncomment unused overrides (noonish)

--- .NET/Microsoft.Recognizers.Text.DateTime/Dutch/Parsers/TimeParser.cs
+++ .NET/Microsoft.Recognizers.Text.DateTime/Dutch/Parsers/TimeParser.cs
@@ -9,43 +9,43 @@ namespace Microsoft.Recognizers.Text.DateTime.Dutch
         {
         }
 
-        protected override DateTimeResolutionResult InternalParse(string text, DateObject referenceTime)
-        {
-            var innerResult = base.InternalParse(text, referenceTime);
-            if (!innerResult.Success)
-            {
-                innerResult = ParseIsh(text, referenceTime);
-            }
+        // protected override DateTimeResolutionResult InternalParse(string text, DateObject referenceTime)
+        // {
+        //     var innerResult = base.InternalParse(text, referenceTime);
+        //     if (!innerResult.Success)
+        //     {
+        //         innerResult = ParseIsh(text, referenceTime);
+        //     }
 
-            return innerResult;
-        }
+        //     return innerResult;
+        // }
 
         // parse "noonish", "11-ish"
-        private DateTimeResolutionResult ParseIsh(string text, DateObject referenceTime)
-        {
-            var ret = new DateTimeResolutionResult();
-            var lowerText = text.ToLowerInvariant();
+        // private DateTimeResolutionResult ParseIsh(string text, DateObject referenceTime)
+        // {
+        //     var ret = new DateTimeResolutionResult();
+        //     var lowerText = text.ToLowerInvariant();
 
-            var match = DutchTimeExtractorConfiguration.IshRegex.MatchExact(lowerText, trim: true);
+        //     var match = DutchTimeExtractorConfiguration.IshRegex.MatchExact(lowerText, trim: true);
 
-            if (match.Success)
-            {
-                var hourStr = match.Groups[Constants.HourGroupName].Value;
-                var hour = Constants.HalfDayHourCount;
-                if (!string.IsNullOrEmpty(hourStr))
-                {
-                    hour = int.Parse(hourStr);
-                }
+        //     if (match.Success)
+        //     {
+        //         var hourStr = match.Groups[Constants.HourGroupName].Value;
+        //         var hour = Constants.HalfDayHourCount;
+        //         if (!string.IsNullOrEmpty(hourStr))
+        //         {
+        //             hour = int.Parse(hourStr);
+        //         }
 
-                ret.Timex = "T" + hour.ToString("D2");
-                ret.FutureValue =
-                    ret.PastValue =
-                        DateObject.MinValue.SafeCreateFromValue(referenceTime.Year, referenceTime.Month, referenceTime.Day, hour, 0, 0);
-                ret.Success = true;
-                return ret;
-            }
+        //         ret.Timex = "T" + hour.ToString("D2");
+        //         ret.FutureValue =
+        //             ret.PastValue =
+        //                 DateObject.MinValue.SafeCreateFromValue(referenceTime.Year, referenceTime.Month, referenceTime.Day, hour, 0, 0);
+        //         ret.Success = true;
+        //         return ret;
+        //     }
 
-            return ret;
-        }
+        //     return ret;
+        // }
     }
 }
